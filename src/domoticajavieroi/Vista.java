a1465dce9c8410968efcd35f8f7b704c191a745f
ocagnen <ocagnen2@gmail.com>
2019-02-10 16:25:22 +0100

    Vista corrección en método

--- src/domoticajavieroi/Vista.java
+++ src/domoticajavieroi/Vista.java
@@ -63,6 +63,7 @@ public class Vista {
                             break;
 
                     }
+                    break;
 
                 case 4:
                     System.out.println("Introduce accion a realizar");
