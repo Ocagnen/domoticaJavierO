fc50441016af978c0a05a6f773bfd72cdb5205ac
Javier <ocagnen2@gmail.com>
2019-02-15 12:23:46 +0100

    Central añadir getters

--- src/domoticajavieroi/Central.java
+++ src/domoticajavieroi/Central.java
@@ -12,10 +12,24 @@ import java.time.LocalDate;
  * @author javier
  */
 public class Central {
-    
+
     private Usuario u;
     private int identificador;
     private LocalDate fechaInstal;
-   
     
+    
+    
+
+    public Usuario getU() {
+        return u;
+    }
+
+    public int getIdentificador() {
+        return identificador;
+    }
+
+    public LocalDate getFechaInstal() {
+        return fechaInstal;
+    }
+
 }
