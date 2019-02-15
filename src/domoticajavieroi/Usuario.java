7d7378c8b9f99eebad44be39640e65ac9cbb0448
Javier <ocagnen2@gmail.com>
2019-02-15 12:11:05 +0100

    Usuario creación constructor param y getters

--- src/domoticajavieroi/Usuario.java
+++ src/domoticajavieroi/Usuario.java
@@ -11,4 +11,21 @@ package domoticajavieroi;
  */
 public class Usuario {
     
+    private String user;
+    private String password;    
+
+    public Usuario(String user, String password) {
+        this.user = user;
+        this.password = password;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    
+    
 }
