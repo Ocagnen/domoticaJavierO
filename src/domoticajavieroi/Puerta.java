e924eddf170cf89961807b86bf54d42cc08dba6d
Javier <ocagnen2@gmail.com>
2019-03-04 12:08:38 +0100

    Creo getters y setters

--- src/domoticajavieroi/Puerta.java
+++ src/domoticajavieroi/Puerta.java
@@ -14,4 +14,30 @@ public class Puerta {
     private boolean estado;
     private int retardo; //segundos ( lo que tarda en abrise/ cerrarse)
     
+    // Java crea un constructor por defecto si no implementamos ninguno
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    public int getRetardo() {
+        return retardo;
+    }
+
+    public void setRetardo(int retardo) {
+        this.retardo = retardo;
+    }
+
+    @Override
+    public String toString() {
+        return "Puerta{" + "estado=" + estado + ", retardo=" + retardo + '}';
+    }
+    
+    
+    
+    
 }
