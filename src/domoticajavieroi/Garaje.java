43fb06eb1d56232c7647019bdf5f9b33d3a910b8
Javier <ocagnen2@gmail.com>
2019-03-04 10:50:09 +0100

    Creo clase Garaje (extend a Estancia)

---
+++ src/domoticajavieroi/Garaje.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package domoticajavieroi;
+
+/**
+ *
+ * @author javier
+ */
+
+//La clase Garaje es un subtipo o subclase de Estancia. Garaje extiende
+// (hereda) a(de) Extancia.
+public class Garaje extends Estancia {
+    
+    private int capacidadVehiculos;
+    
+}
