d3b62b9b9af1afed27e2f8cc363be9eb7c1c4bc7
Javier <ocagnen2@gmail.com>
2019-03-04 11:11:26 +0100

    Creo constructores getters y setters

--- src/domoticajavieroi/Estancia.java
+++ src/domoticajavieroi/Estancia.java
@@ -13,4 +13,27 @@ public class Estancia {
     
     private int m2;
     
+    public Estancia(){
+        m2 = 0;
+    }
+    
+    public Estancia(int m2){
+        this.m2=m2;
+    }
+
+    public int getM2() {
+        return m2;
+    }
+
+    public void setM2(int m2) {
+        this.m2 = m2;
+    }
+
+    @Override
+    public String toString() {
+        return "Estancia{" + "m2=" + m2 + '}';
+    }
+    
+    
+    
 }
