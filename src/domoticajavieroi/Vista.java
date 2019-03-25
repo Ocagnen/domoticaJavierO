4ea61f9169daa4ea6f74d905b8907c79891b7e11
javier <ocagnen2@gmail.com>
2019-03-25 16:56:05 +0100

    Modifica menu para cotrolar errores

--- src/domoticajavieroi/Vista.java
+++ src/domoticajavieroi/Vista.java
@@ -18,21 +18,25 @@ public class Vista {
     public static Comando mostrarMenu() {
 
         int respuesta;
-        //do {
-        System.out.println("¿Que parte del sistema quieres gestionar?");
-        System.out.println("1. Hora");
-        System.out.println("2. Fecha");
-        System.out.println("3. Habitaciones");
-        System.out.println("4. Garaje");
-        System.out.println("5. Consultar estado general del sistema");
-        System.out.println("6. Apagar el sistema");
-        respuesta = tec.nextInt();
+
+        do {
+            System.out.println("¿Que parte del sistema quieres gestionar?");
+            System.out.println("1. Hora");
+            System.out.println("2. Fecha");
+            System.out.println("3. Habitaciones");
+            System.out.println("4. Garaje");
+            System.out.println("5. Consultar estado general del sistema");
+            System.out.println("6. Apagar el sistema");
+            respuesta = tec.nextInt();
+        } while (respuesta > 6 || respuesta < 1);
 
         switch (respuesta) {
             case 1:
-                System.out.println("1. Comprobar hora");
-                System.out.println("2. Cambiar hora");
-                respuesta = tec.nextInt();
+                do {
+                    System.out.println("1. Comprobar hora");
+                    System.out.println("2. Cambiar hora");
+                    respuesta = tec.nextInt();
+                } while (respuesta > 2 || respuesta < 1);
 
                 switch (respuesta) {
                     case 1:
@@ -43,9 +47,11 @@ public class Vista {
                 break;
 
             case 2:
-                System.out.println("1. Comprobar fecha");
+                do {
+                    System.out.println("1. Comprobar fecha");
 
-                respuesta = tec.nextInt();
+                    respuesta = tec.nextInt();
+                } while (respuesta != 1);
 
                 switch (respuesta) {
                     case 1:
@@ -54,30 +60,37 @@ public class Vista {
                 break;
 
             case 3:
-                System.out.println("Selecciona la acción a realizar");
-                System.out.println("1. Gestionar dormitorio");
-                System.out.println("2. Gestionar salon");
-                System.out.println("3. Apagar todas las luces");
-                System.out.println("4. Apagado eco");
-                respuesta = tec.nextInt();
+                do {
+                    System.out.println("Selecciona la acción a realizar");
+                    System.out.println("1. Gestionar dormitorio");
+                    System.out.println("2. Gestionar salon");
+                    System.out.println("3. Apagar todas las luces");
+                    System.out.println("4. Apagado eco");
+                    respuesta = tec.nextInt();
+                } while (respuesta > 4 || respuesta < 1);
 
                 switch (respuesta) {
 
                     case 1:
-                        System.out.println("Selecciona sistema a gestionar");
-                        System.out.println("1. Persiana");
-                        System.out.println("2. Cámara");
-                        System.out.println("3. Luz");
-                        respuesta = tec.nextInt();
+                        do {
+                            System.out.println("Selecciona sistema a gestionar");
+                            System.out.println("1. Persiana");
+                            System.out.println("2. Cámara");
+                            System.out.println("3. Luz");
+                            respuesta = tec.nextInt();
+                        } while (respuesta > 3 || respuesta < 1);
 
                         switch (respuesta) {
                             case 1:
-                                System.out.println("Selecciona acción a realizar");
-                                System.out.println("1. Consultar estado de persiana");
-                                System.out.println("2. Abrir persiana");
-                                System.out.println("3. Cerrar persiana");
-                                System.out.println("4. Dejar persiana a la mitad");
-                                respuesta = tec.nextInt();
+
+                                do {
+                                    System.out.println("Selecciona acción a realizar");
+                                    System.out.println("1. Consultar estado de persiana");
+                                    System.out.println("2. Abrir persiana");
+                                    System.out.println("3. Cerrar persiana");
+                                    System.out.println("4. Dejar persiana a la mitad");
+                                    respuesta = tec.nextInt();
+                                } while (respuesta > 4 || respuesta < 1);
 
                                 switch (respuesta) {
                                     case 1:
@@ -91,11 +104,14 @@ public class Vista {
                                 }
                                 break;
                             case 2:
-                                System.out.println("Selecciona acción a realizar");
-                                System.out.println("1. Consultar estado de vigilancia");
-                                System.out.println("2. Comenzar vigilancia");
-                                System.out.println("3. Finalizar vigilancia");
-                                respuesta = tec.nextInt();
+                                do {
+                                    System.out.println("Selecciona acción a realizar");
+                                    System.out.println("1. Consultar estado de vigilancia");
+                                    System.out.println("2. Comenzar vigilancia");
+                                    System.out.println("3. Finalizar vigilancia");
+                                    respuesta = tec.nextInt();
+                                } while (respuesta > 6 || respuesta < 1);
+
                                 switch (respuesta) {
                                     case 1:
                                         return Comando.CONSULTAR_CAMARA_DORMITORIO;
@@ -106,11 +122,14 @@ public class Vista {
                                 }
                                 break;
                             case 3:
-                                System.out.println("Selecciona acción a realizar");
-                                System.out.println("1. Consultar estado de luz");
-                                System.out.println("2. Encender luz");
-                                System.out.println("3. Apagar luz");
-                                respuesta = tec.nextInt();
+                                do {
+                                    System.out.println("Selecciona acción a realizar");
+                                    System.out.println("1. Consultar estado de luz");
+                                    System.out.println("2. Encender luz");
+                                    System.out.println("3. Apagar luz");
+                                    respuesta = tec.nextInt();
+                                } while (respuesta > 3 || respuesta < 1);
+
                                 switch (respuesta) {
                                     case 1:
                                         return Comando.CONSULTAR_LUZ_DORMITORIO;
@@ -124,21 +143,24 @@ public class Vista {
                         }
 
                     case 2:
-
-                        System.out.println("Selecciona sistema a gestionar");
-                        System.out.println("1. Persiana");
-                        System.out.println("2. Cámara");
-                        System.out.println("3. Luz");
-                        respuesta = tec.nextInt();
+                        do {
+                            System.out.println("Selecciona sistema a gestionar");
+                            System.out.println("1. Persiana");
+                            System.out.println("2. Cámara");
+                            System.out.println("3. Luz");
+                            respuesta = tec.nextInt();
+                        } while (respuesta > 3 || respuesta < 1);
 
                         switch (respuesta) {
                             case 1:
-                                System.out.println("Selecciona acción a realizar");
-                                System.out.println("1. Consultar estado de persiana");
-                                System.out.println("2. Abrir persiana");
-                                System.out.println("3. Cerrar persiana");
-                                System.out.println("4. Dejar persiana a la mitad");
-                                respuesta = tec.nextInt();
+                                do {
+                                    System.out.println("Selecciona acción a realizar");
+                                    System.out.println("1. Consultar estado de persiana");
+                                    System.out.println("2. Abrir persiana");
+                                    System.out.println("3. Cerrar persiana");
+                                    System.out.println("4. Dejar persiana a la mitad");
+                                    respuesta = tec.nextInt();
+                                } while (respuesta > 4 || respuesta < 1);
 
                                 switch (respuesta) {
                                     case 1:
@@ -152,11 +174,14 @@ public class Vista {
                                 }
                                 break;
                             case 2:
-                                System.out.println("Selecciona acción a realizar");
-                                System.out.println("1. Consultar estado de vigilancia");
-                                System.out.println("2. Comenzar vigilancia");
-                                System.out.println("3. Finalizar vigilancia");
-                                respuesta = tec.nextInt();
+                                do {
+                                    System.out.println("Selecciona acción a realizar");
+                                    System.out.println("1. Consultar estado de vigilancia");
+                                    System.out.println("2. Comenzar vigilancia");
+                                    System.out.println("3. Finalizar vigilancia");
+                                    respuesta = tec.nextInt();
+                                } while (respuesta > 3 || respuesta < 1);
+
                                 switch (respuesta) {
                                     case 1:
                                         return Comando.CONSULTAR_CAMARA_SALON;
@@ -167,11 +192,14 @@ public class Vista {
                                 }
                                 break;
                             case 3:
-                                System.out.println("Selecciona acción a realizar");
-                                System.out.println("1. Consultar estado de luz");
-                                System.out.println("2. Encender luz");
-                                System.out.println("3. Apagar luz");
-                                respuesta = tec.nextInt();
+                                do {
+                                    System.out.println("Selecciona acción a realizar");
+                                    System.out.println("1. Consultar estado de luz");
+                                    System.out.println("2. Encender luz");
+                                    System.out.println("3. Apagar luz");
+                                    respuesta = tec.nextInt();
+                                } while (respuesta > 3 || respuesta < 1);
+
                                 switch (respuesta) {
                                     case 1:
                                         return Comando.CONSULTAR_LUZ_SALON;
@@ -183,19 +211,22 @@ public class Vista {
                                 break;
 
                         }
-                        
+
                     case 3:
-                       return Comando.APAGAR_TODAS_LUCES;
+                        return Comando.APAGAR_TODAS_LUCES;
                     case 4:
                         return Comando.APAGADO_ECO;
                 }
 
             case 4:
-                System.out.println("Introduce accion a realizar");
-                System.out.println("1. Comprobar estado de puerta");
-                System.out.println("2. Abrir puerta");
-                System.out.println("3. Cerrar puerta");
-                respuesta = tec.nextInt();
+                do {
+                    System.out.println("Introduce accion a realizar");
+                    System.out.println("1. Comprobar estado de puerta");
+                    System.out.println("2. Abrir puerta");
+                    System.out.println("3. Cerrar puerta");
+                    respuesta = tec.nextInt();
+                } while (respuesta > 3 || respuesta < 1);
+
                 switch (respuesta) {
                     case 1:
                         return Comando.CONSULTAR_PUERTA_GARAJE;
@@ -205,34 +236,33 @@ public class Vista {
                         return Comando.CERRAR_PUERTA_GARAJE;
                 }
                 break;
-            
+
             case 5:
                 return Comando.MOSTRAR_ESTADO_GENERAL;
-                
-                
+
             case 6:
-                return Comando.APAGAR_SISTEMA;               
+                return Comando.APAGAR_SISTEMA;
 
         }
-        
+
         return Comando.APAGAR_SISTEMA;
-               // } while (respuesta != 5);
+        // } while (respuesta != 5);
 
     }
-    
-    public static Usuario loggin(){
+
+    public static Usuario loggin() {
         Scanner tec = new Scanner(System.in);
-        
+
         System.out.println("Introduce nombre de usuario");
         String respuesta = tec.nextLine();
         System.out.println("Introduce contraseña");
         String pass = tec.nextLine();
-        
+
         Usuario u = new Usuario();
-        
+
         u.setNombre(respuesta);
         u.setPassword(pass);
-        
+
         return u;
     }
 }
